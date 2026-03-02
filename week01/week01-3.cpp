8b9e8b8fc18466b22bd19b5438a865295aaa7698
Bieber0125 <13750226@me.mcu.edu.tw>
2026-03-02 11:50:20 +0800

    week01

---
+++ week01/week01-3.cpp
@@ -0,0 +1,11 @@
+///week0-3.cpp
+#include <stdio.h>
+int main()
+{
+    printf("請輸入1個整數: ");
+    int N;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++){
+        printf("Hello World\n");
+    }
+}
